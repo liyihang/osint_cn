46913549e1463f454256c27f2493df845b7bb9aa
lidoudoou <lidoudoou@gmail.com>
2026-03-08 14:03:11 +0800

    Create osint_cn/collection.py with collector classes

---
+++ osint_cn/collection.py
@@ -0,0 +1,21 @@
+class BaseCollector:
+    def collect(self):
+        pass
+
+
+class WeiboCollector(BaseCollector):
+    def collect(self):
+        # Weibo collection logic
+        pass
+
+
+class DouyinCollector(BaseCollector):
+    def collect(self):
+        # Douyin collection logic
+        pass
+
+
+class ZhihuCollector(BaseCollector):
+    def collect(self):
+        # Zhihu collection logic
+        pass
